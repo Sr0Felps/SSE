998d13dfc7d3a37f93064dd79863c98234e12c76
Filipe Costa Junqueira <filipecj0@gmail.com>
2026-06-15 19:56:42 -0300

    feat: finalizando o front

--- src/main/java/com/SSE_Server_Sent_Events/SseServerSentEventsApplication.java
+++ src/main/java/com/SSE_Server_Sent_Events/SseServerSentEventsApplication.java
@@ -2,8 +2,9 @@ package com.SSE_Server_Sent_Events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
+@EnableScheduling
 public class SseServerSentEventsApplication {
 
 	public static void main(String[] args) {
